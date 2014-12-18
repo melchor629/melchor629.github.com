a9ac92a852699789ef218ae9f9cb90ef71e96950
melchor9000 <melchor9000@gmail.com>
2014-12-18 21:40:22 +0100

    Material Design... mas o menos

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 jekyll: jekyll serve
-coffee: coffee --watch --compile --output assets/coffee _coffee/
+coffee: coffee --watch --compile --output assets/js _coffee/
 less: node less-watch
--- less-watch.js
+++ less-watch.js
@@ -6,10 +6,10 @@ fs.watch('./_less/', function(a, b) {
     if(ext === 'less') {
         //lessc -x ./_less/b ./assets/less/b
         var fCss = b.substr(0, b.lastIndexOf('.')) + '.css';
-        var pr = cp.exec('lessc ' + '-x ./_less/' + b +' ./assets/less/' + fCss, function(e, stdout, stderr) {
+        var pr = cp.exec('lessc ' + '-x ./_less/' + b +' ./assets/css/' + fCss, function(e, stdout, stderr) {
             if(stdout) console.log(stdout);
             if(stderr) console.error(stderr);
         });
-        console.log('Compilando \'./_less/' + b + '\' a \'./assets/less/' + fCss +'\'');
+        console.log('Compilando \'./_less/' + b + '\' a \'./assets/css/' + fCss +'\'');
     }
 });
